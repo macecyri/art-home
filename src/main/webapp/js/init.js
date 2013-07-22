c7f989f57f824f5878db806241cb1d39c869496b
macecyri <github@cymace.com>
2013-07-22 23:55:26 +0200

    dd

--- src/main/webapp/js/init.js
+++ src/main/webapp/js/init.js
@@ -1,4 +1,7 @@
 ﻿
+var dialogWidth = $(window).width() * 0.9;
+var dialogHeight = $(window).height() * 0.9;;
+
 /**
  * Script that is executed when the page is loaded
  */
@@ -9,4 +12,30 @@ $(document).ready(function() {
             var value = $(this).text();
             $(this).parents('.dropdown').find('.dropdown-toggle > span').text(value);
         });
+    oTable = $('#search_result_table').dataTable({
+        "bJQueryUI" : true,
+        "bAutoWidth": false,
+        "bPaginate": false,
+        "bInfo": false,
+        "bProcessing": true,
+        "sAjaxSource": '/Users/macecyri/WebstormProjects/art-home/art-home/src/main/webapp/js/arrays.txt'});
+
+    /*****************************Initialisation of Editor dialog box  **********************************/
+    $("#dialog_info").dialog({
+        autoOpen : false,
+        modal : true,
+        resizable : true,
+        draggable : true,
+        autoResize:true ,
+        width : dialogWidth,
+        height : dialogHeight
+    });
+
+    /***************************** Binding 'click' event on a table row (open dialog_box) **********************************/
+    $('#search_result_table').on("click", "tbody tr", function(event) {
+
+        var id_row = oTable.fnGetData(this)[0];
+        openDialogInfoCurrentRow(id_row);
+    });
+
 });
