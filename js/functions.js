e5cdc22841306982ba2dcc186e07c659e15a85ad
macecyri <ac@cymace.com>
2013-08-13 13:57:41 +0200

    relocate

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+﻿/* Get a catalog description and initialize the editor dialog box  */
+function openDialogInfoMember(membertype) {
+    var framePath =    "frames/" + membertype + "_presentation.html"
+    $.ajax({
+        url: framePath,
+        success: function (data) {
+            $("#modal_info").html(data);
+            $("#modal_info").modal('show');
+        },
+        dataType: 'html'
+    });
+
+}
---
+++ js/init.js
@@ -0,0 +1,21 @@
+﻿
+
+/**
+ * Script that is executed when the page is loaded
+ */
+$(document).ready(function() {
+    $('#myTab a:first').tab('show');
+
+    $("a[role='menuitem']").click(
+        function() {
+            var value = $(this).text();
+            var mtype = $(this).data("mtype");
+            $(this).parents('.dropdown').find('.dropdown-toggle > span').text(value);
+            $('#memberType').data("mtype",mtype);
+        });
+
+    /***************************** Binding 'click' event on a the information button (open dialog_box) **********************************/
+    $('#memberType').on("click", "button", function(event) {
+        openDialogInfoMember($('#memberType').data("mtype"));
+    });
+});
